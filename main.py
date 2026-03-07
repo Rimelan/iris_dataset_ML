f932df669a89ae8b2bc66bd560bef32e01a6e557
Ataberk Balaban s286157 <s286157@studenti.polito.it>
2026-03-07 15:20:25 +0100

    added statistical methods mean std var covar and applied for each class of iris

--- main.py
+++ main.py
@@ -50,9 +50,31 @@ def plot_scatter(D,L):
             plt.scatter(D2[x1,:],D2[x2,:],label = 'virginica')
             plt.savefig('hist_%d_%d.pdf' %(x1,x2))
     plt.show()
+def calculate_mean(D):
+    m= np.mean(D,axis=1)
+    return np.array(m).reshape(4,1)
 
 if __name__ == '__main__':
     data,labels = load_data.load_data("iris.csv")
     plot_histogram(data, labels)
     plot_scatter(data,labels)
-    print("hi")
+    mean = calculate_mean(data)
+    var = data.var(1)
+    std = data.std(1)
+    DCent = data - mean
+    covar = (DCent @ DCent.T)/float(data.shape[1])
+    
+    
+    for clss in [0,1,2]:
+        print('Class',clss)
+        DClass = data[:, labels==clss]
+        mean_class= calculate_mean(DClass)
+        print('Mean: ', mean_class)
+        var_class = DClass.var(1)
+        std_class = DClass.std(1)
+        print('Variance: ',var_class) 
+        print('Standart deviation: ',std_class)
+        covar_class = ((DClass-mean_class)@(DClass-mean_class).T)/(float(DClass.shape[1]))
+        print('covar: ', covar_class)
+    
+    
