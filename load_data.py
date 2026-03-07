083d7e17ed3b30c66a34854083e96af6e1f5fb4d
Ataberk Balaban s286157 <s286157@studenti.polito.it>
2026-03-07 13:46:51 +0100

    histogram method added

--- load_data.py
+++ load_data.py
@@ -12,8 +12,9 @@ def load_data(fname):
     with open(fname) as f:
         for line in f:
             curr = line.split(',')[0:-1]
+            curr = (np.array([float(i) for i in curr]))
             curr = np.array(curr).reshape(4,1)
             data.append(curr)
             name = line.split(',')[-1].strip()
             labels.append(hLabels[name])
-    return data,labels
+    return np.hstack(data), np.array(labels, dtype=np.int32)
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import load_data
+
+
+def plot_histogram(D,L):
+    D0=D[:,L == 0]
+    D1=D[:,L == 1]
+    D2=D[:,L == 2]
+    
+    features = {
+        0: 'Sepal length',
+        1: 'Sepal width',
+        2: 'Petal length',
+        3: 'Petal width'
+        }
+    
+    
+    for feID in range(4):
+        plt.figure()
+        plt.xlabel(features[feID])
+        plt.ylabel('Density')
+        plt.hist(D0[feID,:],bins = 10, density= True,label = 'setosa')
+        plt.hist(D1[feID,:],bins = 10, density= True,label = 'versicolor')
+        plt.hist(D2[feID,:],bins = 10, density= True,label = 'virginica')
+        plt.savefig('hist_%d.pdf' %feID)
+    plt.show() 
+    
+
+
 
 if __name__ == '__main__':
     data,labels = load_data.load_data("iris.csv")
+    plot_histogram(data, labels)
     print("hi")
